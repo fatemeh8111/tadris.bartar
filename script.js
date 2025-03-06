7739bbdd24d2209416398898f8bfcae6c356e76e
fatemeh8111 <f.yazdian81@gmail.com>
2025-03-07 00:52:07 +0330

    آپدیت پروژه

--- script.js
+++ script.js
@@ -44,4 +44,11 @@ document.addEventListener("DOMContentLoaded", function() {
         container.appendChild(butterfly);
     }
 });
-
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
